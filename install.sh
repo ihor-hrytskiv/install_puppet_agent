10a0dbfdc843f1ee0abf519fd5e6d79f3d82e51c
ihor.hritskiv <ihor.hritskiv@gmail.com>
2018-06-07 11:14:06 +0300

    fixed errors

--- install.sh
+++ install.sh
@@ -19,29 +19,29 @@ echo "$DISTRIB_RELEASE"
 
 ###Adding the Puppet 5 Platform repository
 if [ "$DIST" == "Ubuntu" ] && [ "$DIST_VER" == "16.04" ]; then
-	echo "The distribution is Ubuntu 16.04"
+	echo "The distribution is Ubuntu 16.04\n"
 	if [ ! -f /etc/apt/sources.list.d/puppet5.list ]; then
-		echo "Adding the Puppet 5 Platform repository"
+		echo "Adding the Puppet 5 Platform repository\n"
 		cd "$TMP_DIR"
 		wget "$PACKAGES_LOCATION$XENIAL_PACKAGE"
 		dpkg -i "$XENIAL_PACKAGE"
 		rm "$XENIAL_PACKAGE"
 		apt update
 	else
-		echo "The Puppet 5 Platform repository already added"
+		echo "The Puppet 5 Platform repository already added\n"
 	fi
 else
-	echo "Unknown Linux distribution, the Puppet 5 Platform repository will not be added"
+	echo "Unknown Linux distribution, the Puppet 5 Platform repository will not be added\n"
 fi
 
 ###Installing the Puppet 5 Platform
 if [ "$DIST" == "Ubuntu" ]; then
 	if [ $(dpkg-query -W -f='${Status}' puppet-agent 2>/dev/null | grep -c "ok installed") == 0 ]; then
-		echo "Installing the Puppet 5 Platform"
+		echo "Installing the Puppet 5 Platform\n"
 		apt-get install puppet-agent
 		/opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true
 		/opt/puppetlabs/bin/puppet agent --test
 	else
-		echo "The Puppet 5 Platform already installed"
+		echo "The Puppet 5 Platform already installed\n"
 	fi
 fi
