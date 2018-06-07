69f44d385531772918a1151d35754a0941310fda
ihor.hritskiv <ihor.hritskiv@gmail.com>
2018-06-07 09:21:52 +0300

    fixed errors

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 ### Settings
 TMP_DIR="/tmp/"
 PACKAGES_LOCATION="https://apt.puppetlabs.com/"
-UBUNTU_16_04_PACKAGE="puppet5-release-xenial.deb"
+XENIAL_PACKAGE="puppet5-release-xenial.deb"
 
 ### Getting OS Distrib
 if [ -f /etc/lsb-release ]; then
@@ -21,8 +21,8 @@ echo "$DISTRIB_RELEASE"
 if [ "$DIST" == "Ubuntu" ] && [ "$DIST_VER" == "16.04" ]; then
 	echo "The distribution is Ubuntu 16.04"
 	cd "$TMP_DIR"
-	wget "$PACKAGES_LOCATION$UBUNTU_16.04_PACKAGE"
-	dpkg -i "$UBUNTU_16.04_PACKAGE"
+	wget "$PACKAGES_LOCATION$XENIAL_PACKAGE"
+	dpkg -i "$XENIAL_PACKAGE"
 	apt update
 else
 	echo "Unknown Linux distribution, the Puppet 5 Platform repository will not be added"
