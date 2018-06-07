1a0a5ea8fd23ac3d80de4cf2424df277d8a8070e
ihor.hritskiv <ihor.hritskiv@gmail.com>
2018-06-07 11:47:28 +0300

    add function addrepoonubuntu

--- install.sh
+++ install.sh
@@ -2,13 +2,14 @@
 ### Settings
 TMP_DIR="/tmp/"
 PACKAGES_LOCATION="https://apt.puppetlabs.com/"
-XENIAL_PACKAGE="puppet5-release-xenial.deb"
+PUPPET_PACKAGE=""
 
 ### Getting OS Distrib
 if [ -f /etc/lsb-release ]; then
 	. /etc/lsb-release
 	DIST="$DISTRIB_ID"
 	DIST_VER="$DISTRIB_RELEASE"
+	DIST_CODENAME="$DISTRIB_CODENAME"
 else
 	DIST="Unknown"
 	DIST_VER="Unknown"
@@ -16,26 +17,17 @@ fi
 
 ###Adding the Puppet 5 Platform repository
 if [ "$DIST" == "Ubuntu" ] && [ "$DIST_VER" == "16.04" ]; then
+	PUPPET_PACKAGE="puppet5-release-$DIST_CODENAME.deb"
 	echo -e "The distribution is Ubuntu 16.04\n"
 	if [ $(dpkg-query -W -f='${Status}' puppet5-release 2>/dev/null | grep -c "ok installed") == 0 ]; then
-		echo -e "Adding the Puppet 5 Platform repository\n"
 		if [ -f /etc/apt/sources.list.d/puppet5.list ]; then
 			rm /etc/apt/sources.list.d/puppet5.list
 		fi
-		cd "$TMP_DIR"
-		wget "$PACKAGES_LOCATION$XENIAL_PACKAGE"
-		dpkg -i "$XENIAL_PACKAGE"
-		rm "$XENIAL_PACKAGE"
-		apt update
+		addrepoonubuntu
 	else
 		if [ ! -f /etc/apt/sources.list.d/puppet5.list ]; then
 			apt remove puppet5-release
-			echo -e "Adding the Puppet 5 Platform repository\n"
-			cd "$TMP_DIR"
-			wget "$PACKAGES_LOCATION$XENIAL_PACKAGE"
-			dpkg -i "$XENIAL_PACKAGE"
-			rm "$XENIAL_PACKAGE"
-			apt update
+			addrepoonubuntu
 		else
 			echo -e "The Puppet 5 Platform repository already added\n"
 		fi
@@ -43,6 +35,16 @@ if [ "$DIST" == "Ubuntu" ] && [ "$DIST_VER" == "16.04" ]; then
 else
 	echo -e "Unknown Linux distribution, the Puppet 5 Platform repository will not be added\n"
 fi
+
+###Addin the Puppet 5 Platform repository on Ubuntu
+function addrepoonubuntu {
+	echo -e "Adding the Puppet 5 Platform repository\n"
+	cd "$TMP_DIR"
+	wget "$PACKAGES_LOCATION$PUPPET_PACKAGE"
+	dpkg -i "$PUPPET_PACKAGE"
+	rm "$PUPPET_PACKAGE"
+	apt update
+}
 
 ###Installing the Puppet 5 Platform
 if [ "$DIST" == "Ubuntu" ]; then
